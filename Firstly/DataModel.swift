3da8affbed6d94c74acef0aa4785d1b09d29bf67
Michael Selsky <Michael@Selsky.me>
2015-10-03 14:31:10 -0400

    Initial commit

---
+++ Firstly/DataModel.swift
@@ -0,0 +1,14 @@
+//
+//  DataModel.swift
+//  Firstly
+//
+//  Created by MichaelSelsky on 10/3/15.
+//  Copyright © 2015 self. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Post: Object {
+    dynamic var imageData: NSData
+}
---
+++ Firstly/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Firstly
+//
+//  Created by MichaelSelsky on 10/3/15.
+//  Copyright © 2015 self. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
