70a62edaf84ed86630b256986507519609de7176
Michael Selsky <mselsky@ispeech.org>
2015-10-03 16:41:42 -0400

    Added a placeholder profile pic

---
+++ Firstly/PostListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostListTableViewCell.swift
+//  Firstly
+//
+//  Created by MichaelSelsky on 10/3/15.
+//  Copyright © 2015 self. All rights reserved.
+//
+
+import UIKit
+
+class PostListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
